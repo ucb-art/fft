0aa23e6b43830f50b18d87288a852c456ac2bd81
Stevo Bailey <stevo.bailey@eecs.berkeley.edu>
2016-12-02 12:04:08 -0800

    [stevo]: still working on rocket style generation

--- src/main/scala/Configs.scala
+++ src/main/scala/Configs.scala
@@ -1,20 +1,43 @@
 package fft
 
-import cde._
-import testchipip.WithSerialAdapter
-import uncore.tilelink.ClientUncachedTileLinkIO
-import rocketchip.PeripheryUtils
-import chisel3._
-import dsptools.numbers.DspReal
+//import cde._
+//import testchipip.WithSerialAdapter
+//import uncore.tilelink.ClientUncachedTileLinkIO
+//import rocketchip.PeripheryUtils
+//import chisel3._
+//import dsptools.numbers.{DspReal, DspComplex, Real}
+//import dsptools.numbers.implicits._
 
-case object BuildDSP extends Field[(Parameters) => Unit]
+import breeze.math.{Complex}
+import breeze.signal.{fourierTr}
+import breeze.linalg._
+import chisel3._
+import chisel3.util._
+import chisel3.iotesters._
+import firrtl_interpreter.InterpreterOptions
+import dsptools.numbers.{DspReal, SIntOrder, SIntRing}
+import dsptools.{DspContext, DspTester, Grow}
+import org.scalatest.{FlatSpec, Matchers}
+import dsptools.numbers.implicits._
+import dsptools.numbers.{DspComplex, Real}
+import scala.util.Random
+import scala.math._
+import org.scalatest.Tag
+
+import cde._
+import junctions._
+import uncore.tilelink._
+import uncore.coherence._
+
+import dsptools._
 
 // create a new DSP Configuration
 class DspConfig extends Config(
   (pname, site, here) => pname match {
-    case BuildDSP => (p: Parameters) => {
-      //import LocalParams._
-      val dsp = Module(new FFT2[DspReal]()(p))
+    case BuildDSP => (q: Parameters) => {
+      //val dsp = Module(new FFT2[DspReal]()(p=p))
+      implicit val p = q
+      val dsp = Module(new FFT(genIn=DspComplex(DspReal(0.0), DspReal(0.0)), config=new FFTConfig(n=8, p=8)))
       ()
     }
     case _ => throw new CDEMatchError
--- src/main/scala/Top.scala
+++ src/main/scala/Top.scala
@@ -3,12 +3,74 @@ package fft
 import chisel3._
 import cde.Parameters
 import diplomacy.{LazyModule, LazyModuleImp}
+import breeze.math.{Complex}
+import breeze.signal.{fourierTr}
+import breeze.linalg._
+import chisel3._
+import chisel3.util._
+import chisel3.iotesters._
+import firrtl_interpreter.InterpreterOptions
+import dsptools.numbers.{DspReal, SIntOrder, SIntRing}
+import dsptools.{DspContext, DspTester, Grow}
+import org.scalatest.{FlatSpec, Matchers}
+import dsptools.numbers.implicits._
+import dsptools.numbers.{DspComplex, Real}
+import scala.util.Random
+import scala.math._
+import org.scalatest.Tag
+
+import cde._
+import junctions._
+import uncore.tilelink._
+import uncore.coherence._
+
+import dsptools._
 
 class DspTop(p: Parameters) extends LazyModule {
-  override lazy val module = new DspTopModule(p, this, new DspTopBundle(p))
+  override lazy val module = Module(new DspTopModule(p, this, new DspTopBundle(p)))
 }
 
 class DspTopBundle(p: Parameters) extends Bundle {}
 
 class DspTopModule[+L <: DspTop, +B <: DspTopBundle](p: Parameters, l: L, b: => B)
-  extends LazyModuleImp(l)
+  extends LazyModuleImp(l) with DspModule {
+    val io = IO(b)
+  }
+
+case object BuildDSP extends Field[(Parameters) => Unit]
+
+trait DspModule {
+  import LocalParams._
+
+  p(BuildDSP)(p)
+}
+
+object LocalParams {
+  def getReal(): DspReal = DspReal(0.0).cloneType
+  implicit val p = Parameters.empty.alter(Map(
+    FFTKey -> FFTConfig(n = 8, p = 8),
+    //NastiId -> "FFT",
+	NastiKey -> NastiParameters(64, 32, 1),
+    PAddrBits -> 32,
+    CacheBlockOffsetBits -> 6,
+    AmoAluOperandBits -> 64,
+    TLId -> "FFT",
+    TLKey("FFT") ->
+        TileLinkParameters(
+          coherencePolicy = new MICoherence(
+            new NullRepresentation(1)),
+          nManagers = 1,
+          nCachingClients = 0,
+          nCachelessClients = 1,
+          maxClientXacts = 4,
+          maxClientsPerPort = 1,
+          maxManagerXacts = 1,
+          dataBeats = 1,
+          dataBits = 64),
+    StreamBlockKey -> new StreamBlockParameters {
+      def genIn [T <: Data] = DspComplex(getReal(), getReal()).asInstanceOf[T]
+      override def genOut[T <: Data] = DspComplex(getReal(), getReal()).asInstanceOf[T]
+      override val lanes = 8
+    }
+  ))
+}
