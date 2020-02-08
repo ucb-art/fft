1cd5f42908ce142887b6966cba0c4f4f0b63d233
Edward Wang <edward.c.wang@compdigitec.com>
2020-02-07 23:54:24 -0500

    Update setup to work around code rot

--- setup.sh
+++ setup.sh
@@ -1,13 +1,35 @@
 #!/bin/bash
 set -ex
 
+PATCHES_DIR="$PWD/patches"
+
 git submodule update --init
 
 cd dsp-framework
 # Don't update riscv-tools since it's obnoxiously big.
 git config --global submodule.riscv-tools.update none
-git submodule update --init --recursive
+git submodule update --init --recursive || true
 git config --global --unset submodule.riscv-tools.update
+
+# Apply patches to hack over versioning hell
+pushd barstools
+git reset --hard HEAD
+git apply $PATCHES_DIR/barstools.patch
+pushd mdf
+git reset --hard HEAD
+git apply $PATCHES_DIR/mdf.patch
+popd
+popd
+
+pushd chisel-testers
+git reset --hard HEAD
+git apply ../../patches/chisel-testers.patch
+popd
+
+pushd dsptools
+git reset --hard HEAD
+git apply ../../patches/dsptools.patch
+popd
 
 ./update.bash no_hwacha
 cd ..
